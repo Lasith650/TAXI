4cc6517d0ff8f8fd8ae96b51f246576a917a37c5
Lasith Manaram Jayalath <lasithmanaram@gmail.com>
2018-04-02 19:01:04 +0530

    After creating the customer register and the driver register pages

--- app/src/main/java/com/example/user/taxii/DriverLoginRegisterActivity.java
+++ app/src/main/java/com/example/user/taxii/DriverLoginRegisterActivity.java
@@ -2,12 +2,49 @@ package com.example.user.taxii;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DriverLoginRegisterActivity extends AppCompatActivity {
+
+    private Button DriverLoginButton;
+    private Button DriverRegisterButton;
+    private TextView DriverRegisterLink;
+    private TextView DriverStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_login_register);
+
+
+        DriverLoginButton = (Button) findViewById(R.id.driver_login_btn);
+        DriverRegisterButton = (Button) findViewById(R.id.driver_register_btn);
+        DriverRegisterLink = (TextView) findViewById(R.id.driver_register_link);
+        DriverStatus = (TextView) findViewById(R.id.driver_status);
+
+
+        DriverRegisterButton.setVisibility(View.INVISIBLE);
+        DriverRegisterButton.setEnabled(false);
+
+
+
+        //if someone click on the customer register link
+        DriverRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //make customerLoginByn & register link invisible
+                DriverLoginButton.setVisibility(View.INVISIBLE);
+                DriverRegisterLink.setVisibility(View.INVISIBLE);
+                DriverStatus.setText("Register Customer");
+
+                DriverRegisterButton.setVisibility(View.VISIBLE);
+                DriverRegisterButton.setEnabled(true);
+
+            }
+        });
     }
 }
