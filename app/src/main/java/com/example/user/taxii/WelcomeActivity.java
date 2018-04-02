7e6db06c937cb587bcbe1e5ca6616deef1f3f074
Lasith Manaram Jayalath <lasithmanaram@gmail.com>
2018-04-02 13:06:11 +0530

    Welcome activity

--- app/src/main/java/com/example/user/taxii/WelcomeActivity.java
+++ app/src/main/java/com/example/user/taxii/WelcomeActivity.java
@@ -1,13 +1,47 @@
 package com.example.user.taxii;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    //initialize the buttons
+    private Button WelcomeDriverButton;
+    private Button WelcomeCustomerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+
+        //casting the buttons
+        WelcomeCustomerButton = (Button) findViewById(R.id.welcome_customer_btn);
+        WelcomeDriverButton = (Button) findViewById(R.id.welcome_driver_btn);
+
+        //setting the onclick listener for the buttons
+        WelcomeCustomerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent LoginRegisterCustomerIntent = new Intent(WelcomeActivity.this, CustomerLoginRegisterActivity.class);
+                startActivity(LoginRegisterCustomerIntent);
+            }
+
+        });
+
+        WelcomeDriverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent LoginRegisterDriverIntent = new Intent(WelcomeActivity.this, DriverLoginRegisterActivity.class);
+                startActivity(LoginRegisterDriverIntent);
+            }
+
+        });
     }
 }
